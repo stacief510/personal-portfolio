46bf2f061787cebc023ab49c3f7113fdb9b58f03
stacief510 <staciemfraser@gmail.com>
2018-03-25 11:12:27 -0700

    skills update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,20 @@
-// sanity check
-console.log("main.js linked.");
-//when click on contact, slide down & out module with all contact links
-//make H1 stop when hits nav Bar
+ $(document).ready(function() {
+    console.log( "ready!" );
 
+$(".circle").mouseenter(function(){
+    $('span').toggle().hide();
+        $('.altSpan').toggle().show();
+//$('.misc').append(`<li>"AJAX"</li> <li>Bootstrap</li> <li>Git&GitHub</li> <li>MongoDB</li> <li>Node</li> <li>React</li>`)
+});
+
+$(".circle").mouseleave(function(){
+    $('span').toggle().show();
+        $('.altSpan').toggle().hide();
+});
+
+
+
+
+
+
+ }); //docReady Close
